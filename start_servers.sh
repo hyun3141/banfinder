f6c03ec3a1883407f67045864b3c6eceeb90ce34
hyun3141 <msh7383@gmail.com>
2025-10-10 16:42:11 +0900

    Delete start_servers.sh

--- start_servers.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# FastAPI 서버 백그라운드 실행
-uvicorn pages.api:app --host 0.0.0.0 --port 8080
-
-# Streamlit 서버 포그라운드 실행
-#streamlit run streamlit_ui.py --server.port=8080 --server.address=0.0.0.0
